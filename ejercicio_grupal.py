6ef812b319a1385635f2aa97b2611296cadc6f2b
Julio QM <julioquinchichester@gmail.com>
2025-10-19 09:40:44 -0600

    Cambio de codificacion

--- ejercicio_grupal.py
+++ ejercicio_grupal.py
@@ -5,16 +5,38 @@ El objetivo es detectar, registrar, corregir y documentar cada incidencia.
 
 import logging
 
+logging.basicConfig(
+    filename="app.log",                    # Guarda los logs en un archivo (opcional)
+    level=logging.INFO,                    # Nivel mínimo que se mostrará
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Formato del mensaje
+    datefmt="%Y-%m-%d %H:%M:%S"            # Formato de la fecha/hora
+)
+
+
+""" logging.info("Inicio del proceso")
+logging.warning("Archivo no encontrado, usando ruta alternativa")
+logging.error("No se pudo conectar a la base de datos") """
+
+
 def dividir(a, b):
-    """Devuelve la división de dos números."""
-    return a / b  
+    try:
+        resultado = a / b
+        logging.info(f"División realizada: {a} / {b} = {resultado}")
+        return resultado
+    except ZeroDivisionError as e:
+        logging.error(f"Error de división: {e}")
+        return None
 
 def promedio(lista_numeros):
     """Calcula el promedio de una lista de números."""
-    total = 0
-    for n in lista_numeros:
-        total += n
-    return total / len(lista_numeros)  
+    try:
+        total = 0
+        for n in lista_numeros:
+            total += n
+        return total / len(lista_numeros)  
+    except ZeroDivisionError as e:
+        logging.error(f"Error de divion:{e}")
+        return None
 
 def obtener_elemento(lista, indice):
     """Devuelve un elemento de la lista según el índice indicado."""
@@ -22,10 +44,14 @@ def obtener_elemento(lista, indice):
 
 def calcular_total(precios):
     """Suma los precios de una lista."""
-    total = 0
-    for p in precios:
-        total += p
-    return total  
+    try:
+        total = 0
+        for p in precios:
+            total += p
+        return total  
+    except ZeroDivisionError as e:
+        logging.error(f"Error de divion:{e}")
+        return None
 
 # ------------------------------------------------------
 # FUNCIÓN PRINCIPAL
@@ -34,17 +60,17 @@ def calcular_total(precios):
 def main():
     logging.info("Inicio del programa")
 
-    resultado_div = dividir(10, 0)
+    resultado_div = dividir(10, 1)
     print("Resultado de la división:", resultado_div)
 
-    datos = []
+    datos = [1,2]
     print("Promedio:", promedio(datos))
 
     lista = [1, 2, 3]
-    print("Elemento:", obtener_elemento(lista, 5))
+    print("Elemento:", obtener_elemento(lista, 2)) # corregido
 
     
-    precios = [10, 20, "treinta", 40]
+    precios = [10, 20, 30, 40] ## coloca numeros enteros
     print("Total de precios:", calcular_total(precios))
 
     logging.info("Fin del programa")
